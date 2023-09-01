d88b3cf084429edcc340d509c99f58891a26c8e2
Tomi Prasetyo <tomi.prasetyo95@gmail.com>
2023-09-01 14:40:53 +0700

    konfigurasi laravel spatie

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles, HasScope;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
